ceb44b99d9f7532387cbd756a0b44d775f85ac5e
Stephanie Viccari <stephviccari@gmail.com>
2018-10-01 17:29:20 -0400

    Wires up the RSVP link and the See All Events link
    Summary:
    We want the process of RSVP'ing and viewing all upcoming events to be as
    easy as possible.
    
    This PR:
    Updates the RSVP link to direct the user to the meetup event where they
    can RSVP for the event.
    Updates "See all events" link to direct the user to meetup page that
    shows all upcoming events.

--- spec/views/homes/_events_view_spec.rb
+++ spec/views/homes/_events_view_spec.rb
@@ -1,11 +1,13 @@
-RSpec.describe "homes/_events.html.erb" do
+RSpec.describe "homes/_events.html.erb", type: :view do
   context "when the next event is unknown" do
     it "display that the upcoming event is unknown" do
       assign(:upcoming_event, UnknownMeetupEvent.new)
 
       render
 
-      expect(rendered).to match("There are no upcoming events.")
+      within_role("upcoming-event-details") do
+        expect(rendered).to have_text("There are no upcoming events.")
+      end
     end
   end
 end
